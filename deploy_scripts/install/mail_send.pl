3405969d6ab67050b055285b7ff613792205096d
Laguta Sergey <lagutas@gmail.com>
2015-07-15 12:26:17 +0500

    add mail send

---
+++ deploy_scripts/install/mail_send.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+use Logic::Tools;
+use Getopt::Long;
+use Cwd;
+
+use strict;
+
+my $my_dir = getcwd;
+
+
+
+my ($emails,$theme,$message,$path);
+$message=undef;
+
+GetOptions( "emails=s"   => \$emails, 
+            "theme=s"    => \$theme,
+            "message=s"  => \$message,
+            "path=s"     => \$path);
+
+
+my $tools=Logic::Tools->new(logfile => $my_dir.'/'.$path.'/deploy.log');
+
+$tools->logprint("info","send mail: $emails: [$theme] $message");
+
+chomp(my $hostname=`hostname -s`);
+
+chomp(my $check_mutt=`whereis -b mutt | grep ": /" | wc -l`);
+    
+my $mail_sender;
+if($check_mutt==1)
+{
+    $mail_sender='mutt';
+}
+else
+{
+    $mail_sender='mail';   
+}
+
+if(defined($emails))
+{
+    foreach(split(",",$emails))
+    {        
+        `echo "$message" | $mail_sender -s "$hostname: $theme" $_`;
+    }
+}
+else
+{
+    `echo "$message" | $mail_sender -s "$hostname:INFO $theme" root`;
+}
--- deploy_scripts/install/test_install_script.pl
+++ deploy_scripts/install/test_install_script.pl
@@ -25,11 +25,11 @@ GetOptions( "path=s"                => \$path,
 
 my $tools=Logic::Tools->new(logfile => $my_dir.'/'.$path.'/deploy.log');
 
-$tools->logprint("info","unit test: $script_name");
+$tools->logprint("info","unit test [$script_name]: $script_name");
 
-$tools->logprint("info","unit test: read test dir $test_dir");
+$tools->logprint("info","unit test [$script_name]: read test dir $test_dir");
 
-opendir(my $test_dir_hdl,$test_dir) || $tools -> logprint("error","не удалось открыть каталог $test_dir");
+opendir(my $test_dir_hdl,$test_dir) || $tools -> logprint("error","unit test [$script_name]:не удалось открыть каталог $test_dir");
 
 my @tests=readdir($test_dir_hdl);
 
@@ -57,78 +57,88 @@ mkdir($my_dir.'/'.$src_script_dir.'/testlog');
 foreach(@test_num)
 {
     my $command='sudo perl '.$my_dir.'/'.$test_dir.'/'.$tests{$_}.' '.$my_dir.'/'.$src_script_dir.'/script'.' 1>>'.$my_dir.'/'.$src_script_dir.'/testlog/'.$tests{$_}.'.log'.' 2>>'.$my_dir.'/'.$src_script_dir.'/testlog/'.$tests{$_}.'.log';
-    $tools->logprint("info","unit test: $tests{$_} run $command");
+    $tools->logprint("info","unit test [$script_name]: $tests{$_} run $command");
     `$command`;
     my $test_name=$tests{$_};
     open(my $test_log,"<",$my_dir.'/'.$src_script_dir.'/testlog/'.$tests{$_}.'.log');
     my $error_count=0;
+    my @message;
     while (<$test_log>) 
     {
         chomp;
-        $tools->logprint("info","unit test: $test_name - $_");
+        $tools->logprint("info","unit test [$script_name]: $test_name - $_");
         unless($_=~/^ok.+/||$_=~/^\d+.+\d+$/)
         {
             print $_."\n";
-            $tools->logprint("error","unit test: $test_name - $_");
+            $tools->logprint("error","unit test [$script_name]: $test_name - $_");
+            push(@message,$_);
             $error_count++;
         }
     }
     close($test_log);
     if($error_count>0)
     {
-        $tools->logprint("error","project has many error: $error_count");
-        mail_send($emails,"project has many error: $error_count");
+        $tools->logprint("error","unit test [$script_name]: project has many error: $error_count");
+        my $command=$path.'/install/mail_send.pl'.
+                                ' -emails '.$emails.
+                                ' -theme '.'"project has many error: '.$error_count.'"'.
+                                ' -message "'.join("\n",@message.'"'.
+                                ' -path '.$path);
+        $tools->logprint("info","unit test [$script_name]: send mail $command");
+        `$command`;
         print -1;
         exit;
     }
 }
 
 my $command=$path.'/install/20_install_projects.pl '.$src_script_cfg_dir.'/'.$script_cfg_name.' '.$dst_script_cfg_dir.'/'.$script_cfg_name.' '.$path;
-$tools->logprint("info","unit test: install cfg $command");
-my $cfg_install=`$command`; if($cfg_install<0) 
+$tools->logprint("info","unit test [$script_name]: install cfg $command");
+my $cfg_install=`$command`; 
+if($cfg_install<0) 
 { 
-    $tools->logprint("error","error install cfg file");
-    mail_send($emails,"error install cfg file");
+    $tools->logprint("error","unit test [$script_name]: error install cfg file");
+    my $command=$path.'/install/mail_send.pl'.
+                                ' -emails '.$emails.
+                                ' -theme '.'"error install cfg file"'.
+                                ' -path '.$path;
+    $tools->logprint("info","unit test [$script_name]: send mail $command");
+    `$command`;
+    print -1;
     exit;
 }
 
 my $command=$path.'/install/20_install_projects.pl '.$src_script_dir.'/script/'.$script_name.' '.$dst_script_dir.'/'.$script_name.' '.$path;
-$tools->logprint("info","unit test: install $command");
-my $script_install=`$command`; if($script_install<0) 
+$tools->logprint("info","unit test [$script_name]: install $command");
+my $script_install=`$command`; 
+if($script_install<0) 
 { 
-    $tools->logprint("error","error install script");
-    mail_send($emails,"error install script"); 
+    $tools->logprint("error","unit test [$script_name]: error install script");
+    my $command=$path.'/install/mail_send.pl'.
+                                ' -emails '.$emails.
+                                ' -theme '.'"error install script"'.
+                                ' -path '.$path;
+    $tools->logprint("info","unit test [$script_name]: send mail $command");
+    `$command`;
+    print -1;
+    exit;
+}
+
+my $command='sudo chmod +x '.$dst_script_dir.'/'.$script_name;
+my $chmod=`$command`; if($chmod ne undef) 
+{
+    $tools->logprint("error","unit test [$script_name]: $chmod");
+    print -1;
     exit;
 }
 
 
 if($cfg_install>0||$script_install>0)
 {
-    $tools->logprint("info","unit test: need script restart");
+    $tools->logprint("info","unit test [$script_name]: need script restart");
     print 1;
 }
 else
 {
-    $tools->logprint("info","unit test: not need script restart");
+    $tools->logprint("info","unit test [$script_name]: not need script restart");
     print 0;
-}
-
-
-sub mail_send
-{
-    my $emails=shift;
-    my $theme=shift;
-    chomp(my $hostname=`hostname -s`);
-        
-    if(defined($emails))
-    {
-        foreach(split(",",$emails))
-        {
-            `echo "" | mutt -s "$hostname: $theme" $_`;
-        }
-    }
-    else
-    {
-        `echo "" | mutt -s "$hostname:INFO $theme" root`;
-    }
 }
