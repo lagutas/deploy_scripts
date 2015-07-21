7a95925a0d70b29c3c5149c0c7995482783ad8a7
Laguta Sergey <lagutas@gmail.com>
2015-07-21 18:41:08 +0500

    kamailio tests

---
+++ t/kamailio/20_check_kamailio.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $emails=shift;
+
+
+my $kamailio_pid_file;
+my $pid;
+if( -e "/var/run/kamailio.pid")
+{
+	$kamailio_pid_file = "/var/run/kamailio.pid";
+}
+elsif( -e "/var/run/kamailio/kamailio.pid")
+{
+	$kamailio_pid_file = "/var/run/kamailio/kamailio.pid";
+}
+
+
+
+open(my $pid_file,'<',$kamailio_pid_file) || die "ERROR: can't open file";
+my $pid=<$pid_file>;
+close $pid_file;
+chomp $pid;
+        
+unless( -e "/proc/$pid" )
+{
+    send_error($emails,"kamailio not run");
+    my $command=$path.'/install/mail_send.pl'.
+                                ' -emails '.$emails.
+                                ' -theme '.'"[kamailio deploy]: test after reboot"'.
+                                ' -path '.$path.
+                                ' -message '.'error: kamailio not run';
+    $tools->logprint("info","exec $command");
+    `$command`;
+}
+
+my $ctl_error;
+unless( -e "/var/run/kamailio/kamailio_ctl" )
+{
+    my $command=$path.'/install/mail_send.pl'.
+                                ' -emails '.$emails.
+                                ' -theme '.'"[kamailio deploy]: test after reboot"'.
+                                ' -path '.$path.
+                                ' -message '.'"/var/run/kamailio/kamailio_ctl not exist"';
+    $tools->logprint("info","exec $command");
+    `$command`;
+    if($ctl_error!=2)
+    {
+        $ctl_error=1;
+        print "error: /var/run/kamailio/kamailio_ctl not exist\n";    
+    }
+}
+
+my $fifo_error;
+unless( -e "/var/run/kamailio/kamailio_fifo" )
+{
+    my $command=$path.'/install/mail_send.pl'.
+                                ' -emails '.$emails.
+                                ' -theme '.'"[kamailio deploy]: test after reboot"'.
+                                ' -path '.$path.
+                                ' -message '.'"/var/run/kamailio/kamailio_fifo not exist"';
+    $tools->logprint("info","exec $command");
+    `$command`;
+    if($fifo_error!=2)
+    {
+        $fifo_error=1;
+        print "error: /var/run/kamailio/kamailio_fifo not exist\n";
+    }
+}
+
+if($ctl_error==1)
+{
+    sleep(3);
+    my $command=$path.'/install/mail_send.pl'.
+                                ' -emails '.$emails.
+                                ' -theme '.'"[kamailio deploy]: test after reboot"'.
+                                ' -path '.$path.
+                                ' -message '.'"ctl not be created, need restart again"';
+    $tools->logprint("info","exec $command");
+    `$command`;
+    $ctl_error=2;
+}
+
+if($fifo_error==1)
+{
+    sleep(3);
+    my $command=$path.'/install/mail_send.pl'.
+                                ' -emails '.$emails.
+                                ' -theme '.'"[kamailio deploy]: test after reboot"'.
+                                ' -path '.$path.
+                                ' -message '.'"fifo not be created, need restart again"';
+    $tools->logprint("info","exec $command");
+    `$command`;
+    $fifo_error=2;
+}
