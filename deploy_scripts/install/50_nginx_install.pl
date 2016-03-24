4852f560d630a9d57b5dd70f6468fb3fe5706666
juls14 <juls@itlogic.pro>
2016-03-24 18:16:39 +0500

    add nginx deploy script

---
+++ deploy_scripts/install/50_nginx_install.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+use strict;
+use Logic::Tools;
+
+my $path=shift;
+
+
+my $tools=Logic::Tools->new(logfile => 'Syslog');
+
+$tools->logprint("info","start nginx deploy");
+
+my $command='sudo aptitude -y install nginx';
+my $exec_command=`$path/install/30_exec_command.pl '$command' $path`; if($exec_command ne undef) {  die $exec_command."\n"; }
+
+$command='sudo echo "include /etc/nginx/conf.d/*.conf;" >> /etc/nginx/nginx.conf';
+$exec_command=`$path/install/30_exec_command.pl '$command' $path`; if($exec_command ne undef) {  die $exec_command."\n"; }
+
+opendir(my $nginx_conf,$path.'/etc/nginx/conf.d/') || $tools -> logprint("error","не удалось открыть каталог $path/etc/nginx/conf.d");
+    
+my @nginx_conf = readdir($nginx_conf);
+
+my $check;
+foreach my $conf (@nginx_conf) 
+{
+    my $copy_nginx_config=`$path/install/20_install_projects.pl $path/etc/nginx/conf.d/$conf /etc/nginx/nginx_conf/$conf $path`; if($copy_nginx_config<0) {  die "error install file\n"; exit; }
+    $check=$check+$copy_nginx_config;
+}
+
+closedir $nginx_conf;
+
+if($check>0) {
+	$command='sudo /etc/init.d/nginx reload';
+	$exec_command=`$path/install/30_exec_command.pl '$command' $path`; if($exec_command ne undef) {  die $exec_command."\n"; }
+}
+
+
+
+
