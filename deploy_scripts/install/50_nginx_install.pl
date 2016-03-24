f8a475d9d942fbcda79784800b8088cb5a2d11d3
juls14 <juls@itlogic.pro>
2016-03-24 18:38:10 +0500

    Update 50_nginx_install.pl

--- deploy_scripts/install/50_nginx_install.pl
+++ deploy_scripts/install/50_nginx_install.pl
@@ -10,11 +10,21 @@ my $tools=Logic::Tools->new(logfile => 'Syslog');
 
 $tools->logprint("info","start nginx deploy");
 
-my $command='sudo aptitude -y install nginx';
-my $exec_command=`$path/install/30_exec_command.pl '$command' $path`; if($exec_command ne undef) {  die $exec_command."\n"; }
+chomp(my $check_nginx=`sudo dpkg-query -W nginx 2>/dev/null`);
+$tools->logprint("info","check nginx $check_nginx");
 
-$command='sudo echo "include /etc/nginx/conf.d/*.conf;" >> /etc/nginx/nginx.conf';
-$exec_command=`$path/install/30_exec_command.pl '$command' $path`; if($exec_command ne undef) {  die $exec_command."\n"; }
+unless($check_nginx=~/nginx/)
+{
+	my $command='sudo aptitude -y install nginx';
+	my $exec_command=`$path/install/30_exec_command.pl '$command' $path`; if($exec_command ne undef) {  die $exec_command."\n"; }
+	
+	$command='sudo echo "include /etc/nginx/conf.d/*.conf;" >> /etc/nginx/nginx.conf';
+	$exec_command=`$path/install/30_exec_command.pl '$command' $path`; if($exec_command ne undef) {  die $exec_command."\n"; }
+}
+else
+{
+	$tools->logprint("info","nginx is already installed");
+}
 
 opendir(my $nginx_conf,$path.'/etc/nginx/conf.d/') || $tools -> logprint("error","не удалось открыть каталог $path/etc/nginx/conf.d");
     
