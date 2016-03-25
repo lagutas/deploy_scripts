fa774a763061cb13e418272402c3c684c4569d59
juls14 <juls@itlogic.pro>
2016-03-25 10:04:45 +0500

    Update 50_nginx_install.pl

--- deploy_scripts/install/50_nginx_install.pl
+++ deploy_scripts/install/50_nginx_install.pl
@@ -17,23 +17,23 @@ unless($check_nginx=~/nginx/)
 {
     my $command='sudo aptitude -y install nginx';
     my $exec_command=`$path/install/30_exec_command.pl '$command' $path`; if($exec_command ne undef) {  die $exec_command."\n"; }
-    
-    my $command='sudo echo "include /etc/nginx/conf.d/*.conf;" >> /etc/nginx/nginx.conf';
-    my $exec_command=`$path/install/30_exec_command.pl '$command' $path`; if($exec_command ne undef) {  die $exec_command."\n"; }
 }
 else
 {
     $tools->logprint("info","nginx is already installed");
 }
 
+my $check;
+my $copy_nginx_config=`$path/install/20_install_projects.pl $path/etc/nginx/nginx.conf /etc/nginx/nginx.conf $path`; if($copy_nginx_config<0) {  die "error install file\n"; exit; }
+$check=$copy_nginx_config;
+
 opendir(my $nginx_conf,$path.'/etc/nginx/conf.d/') || $tools -> logprint("error","не удалось открыть каталог $path/etc/nginx/conf.d");
     
 my @nginx_conf = readdir($nginx_conf);
 
-my $check;
 foreach my $conf (@nginx_conf) 
 {
-    my $copy_nginx_config=`$path/install/20_install_projects.pl $path/etc/nginx/conf.d/$conf /etc/nginx/nginx_conf/$conf $path`; if($copy_nginx_config<0) {  die "error install file\n"; exit; }
+    my $copy_nginx_config=`$path/install/20_install_projects.pl $path/etc/nginx/conf.d/$conf /etc/nginx/conf.d/$conf $path`; if($copy_nginx_config<0) {  die "error install file\n"; exit; }
     $check=$check+$copy_nginx_config;
 }
 
